d293b4ea65e3e8b62ad0f65ab160c729ca59c56d
JIA JIE CHAI <101003895+JJCHAI14@users.noreply.github.com>
2026-06-18 12:22:52 +0800

    Update __init__.py
    Added module docstring and updated __all__.

--- src/indexer/__init__.py
+++ src/indexer/__init__.py
@@ -1,1 +1,12 @@
+# src/indexer/__init__.py
 
+"""
+Indexer Module
+Transforms raw string tokens into structural TF-IDF numerical vector spaces.
+"""
+
+from .tfidf2 import compute_tfidf
+
+__all__ = [
+    "compute_tfidf"
+]
