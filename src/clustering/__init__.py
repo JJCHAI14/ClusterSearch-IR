ddd427e014cd5fac989c8aaa389f5315b88d4f81
JIA JIE CHAI <101003895+JJCHAI14@users.noreply.github.com>
2026-06-18 12:22:03 +0800

    Writing __init__.py for clustering

--- src/clustering/__init__.py
+++ src/clustering/__init__.py
@@ -1,1 +1,19 @@
+# src/clustering/__init__.py
 
+"""
+Clustering Module
+Implements custom vector segmentation, cluster analysis, performance verification, 
+and graphical scatter distributions.
+"""
+
+from .kmeans2 import kmeans_from_scratch
+from .clusterAna import analyze_cluster_terms
+from .evaluation import evaluate_clustering
+from .visualization import visualize_clusters
+
+__all__ = [
+    "kmeans_from_scratch",
+    "analyze_cluster_terms",
+    "evaluate_clustering",
+    "visualize_clusters"
+]
