301d96875db3f521c979235da896881d6dd55e35
JIA JIE CHAI <101003895+JJCHAI14@users.noreply.github.com>
2026-06-18 12:20:04 +0800

    Writing __init__.py for crawler

--- src/crawler/__init__.py
+++ src/crawler/__init__.py
@@ -1,1 +1,14 @@
+# src/crawler/__init__.py
 
+"""
+Crawler Module
+Handles concurrent seed extraction, multi-threaded crawling, and raw payload serialization.
+"""
+
+from .crawler2 import read_seed_urls, crawl, save_to_json
+
+__all__ = [
+    "read_seed_urls",
+    "crawl",
+    "save_to_json"
+]
